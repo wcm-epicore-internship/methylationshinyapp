070180295c3b9e935bcb8aacb646cbacd5b37220
paige-lee <66644787+paige-lee@users.noreply.github.com>
2020-07-23 22:02:58 -0400

    Update Plots.R

--- Plots.R
+++ Plots.R
@@ -4,7 +4,7 @@ small_matrix_4 <- errbs5916_site_coverage_mat[1:1000000, 1:2] #create smaller ma
 two_columns_4 <- as_tibble(small_matrix_4) %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=10)
 #put this data into two columns "Sample" and "Cpg.Coverage," drop NAs, filter it for at least 10x coverage
 ggplot(two_columns_4, aes(x=Cpg.Coverage, color = Sample)) + geom_density() + xlim(c(0,1000))
-#density plot of the CpG coverage of the first two samples/columns of errbs5916; most sites are covered 0-250x.
+#density plot of the CG coverage of the first two samples/columns of errbs5916; most sites are covered 0-250x.
 save(two_columns_4, file = "errbs5916_first_two_samples_10x_noNA.rda") #make the data easier to load
 
 small_matrix_1 <- errbs5916_site_coverage_mat[1:1000000, 1:10] #make a smaller matrix from errbs5916
@@ -15,8 +15,8 @@ two_columns <- as_tibble(small_matrix_1) %>% gather(Sample, Cpg.Coverage) %>% dr
 ggplot(two_columns, aes(x=Sample, y=Cpg.Coverage)) + geom_boxplot(outlier.size = -1) + coord_flip(ylim = c(0,500))
 #make a ggplot with this data, using Sample as independent variable and Cpg.Coverage as dependent variable
 #the geom_boxplot function makes the ggplot a boxplot, and you can give the size of outliers
-#coord_flip rotates the boxplot and sets the domain for CpG.Coverage
-#From the boxplots, we can see the median CpG coverage in most of the 10 samples is between 0 and 200 
+#coord_flip rotates the boxplot and sets the domain for CG.Coverage
+#From the boxplots, we can see the median CG coverage in most of the 10 samples is between 0 and 200 
 save(two_columns, file = "errbs5916_first_ten_samples_10x_noNA.rda") #make the data easier to load
 
 library(tidyverse)
@@ -58,10 +58,62 @@ test6
 
 #Bind test1, test2, test3, test4, test5, test6
 p <- bind_rows(test1, test2, test3, test4, test5, test6)
-p
+p 
 ggplot(p, aes(x=Project, y=Count)) + geom_boxplot() + coord_flip()
-#Boxplots of each project, comparing their numbers of CpG sites with at least 10x coverage
+#Boxplots of each project, comparing their numbers of CG sites with at least 10x coverage
 save(p, file = "all_projects.sample_count_mean.rda")
 
 ggplot(p, aes(x=Project, y=Mean)) + geom_boxplot() + coord_flip()
-#Boxplots of each project, comparing their mean CpG coverage with at least 10x coverage
+#Boxplots of each project, comparing their mean CG coverage with at least 10x coverage
+
+ggplot(p, aes(x=Project, y=Count)) + geom_point(size=2, aes(color = Project)) + coord_flip() + 
+  stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 0.5)
+#scatterplot of the number of CG units with at least 10x coverage in each project with the medians for each shown
+
+ggplot(p, aes(x=Project, y=Count)) + geom_dotplot(binaxis="y", stackdir = "center", dotsize=0.3, aes(fill = Project)) + 
+  coord_flip() + stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 0.5)
+#dot plot of the number of CG units with at least 10x coverage in each project with the medians for each shown
+
+ggplot(p, aes(x=Project, y=Mean)) + geom_dotplot(binaxis="y", stackdir = "center", dotsize=0.3, aes(fill = Project)) + 
+  coord_flip() + stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 0.5)
+#dot plot of the mean number of CG units with at least 10x coverage in each project with the medians for each shown
+
+rrbs5910_5x_noNA <- rrbs5910 %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=5) 
+rrbs6071_5x_noNA <- rrbs6071 %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=5) 
+rrbs6125_5x_noNA <- rrbs6125 %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=5) 
+errbs6121_5x_noNA <- errbs6121 %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=5)
+errbs5975_5x_noNA <- errbs5975 %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=5) 
+errbs5916_5x_noNA <- errbs5916 %>% gather(Sample, Cpg.Coverage) %>% drop_na() %>% filter(Cpg.Coverage >=5) 
+
+test7 <- rrbs5910_5x_noNA %>% group_by(Sample) %>% summarize(Count=n(), Mean=mean(Cpg.Coverage)) %>% 
+  mutate(Project=rep("rrbs5910", length(Sample)))
+test8 <- rrbs6071_5x_noNA %>% group_by(Sample) %>% summarize(Count=n(), Mean=mean(Cpg.Coverage)) %>%
+  mutate(Project=rep("rrbs6071", length(Sample)))
+test9 <- rrbs6125_5x_noNA %>% group_by(Sample) %>% summarize(Count=n(), Mean=mean(Cpg.Coverage)) %>%
+  mutate(Project=rep("rrbs6125", length(Sample)))
+test10 <- errbs5916_5x_noNA %>% group_by(Sample) %>% summarize(Count=n(), Mean=mean(Cpg.Coverage)) %>%
+  mutate(Project=rep("errbs5916", length(Sample)))
+test11 <- errbs5975_5x_noNA %>% group_by(Sample) %>% summarize(Count=n(), Mean=mean(Cpg.Coverage)) %>%
+  mutate(Project=rep("errbs5975", length(Sample)))
+test12 <- errbs6121_5x_noNA %>% group_by(Sample) %>% summarize(Count=n(), Mean=mean(Cpg.Coverage)) %>%
+  mutate(Project=rep("errbs6121", length(Sample)))
+q <- bind_rows(test7, test8, test9, test10, test11, test12)
+save(q, file = "all_projects.sample_count_mean_5x.rda")
+
+ggplot(q, aes(x=Project, y=Count)) + geom_boxplot() + coord_flip()
+#Boxplots of each project, comparing their numbers of CG sites with at least 5x coverage
+
+ggplot(q, aes(x=Project, y=Mean)) + geom_boxplot() + coord_flip()
+#Boxplots of each project, comparing their mean CG coverage with at least 5x coverage
+
+ggplot(q, aes(x=Project, y=Count)) + geom_point(size=2, aes(color = Project)) + coord_flip() + 
+  stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 0.5)
+#scatterplot of the number of CG units with at least 5x coverage in each project with the medians for each shown
+
+ggplot(q, aes(x=Project, y=Count)) + geom_dotplot(binaxis="y", stackdir = "center", dotsize=0.3, aes(fill = Project)) + 
+  coord_flip() + stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 0.5)
+#dot plot of the number of CG units with at least 5x coverage in each project with the medians for each shown
+
+ggplot(q, aes(x=Project, y=Mean)) + geom_dotplot(binaxis="y", stackdir = "center", dotsize=0.3, aes(fill = Project)) + 
+  coord_flip() + stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", width = 0.5)
+#dot plot of the mean number of CG units with at least 5x coverage in each project with the medians for each shown
