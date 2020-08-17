5aacb096cb11c159f8e7b5e84f96469b5a3bb35d
paige-lee <66644787+paige-lee@users.noreply.github.com>
2020-08-16 20:58:19 -0400

    Add files via upload

---
+++ Code for making 5x overlap plot.R
@@ -0,0 +1,43 @@
+#ADDING CG SITE AS A COLUMN TO ALL RRBS DATA TIBBLES
+rrbs5910.df <- as.data.frame(rrbs5910_site_coverage_mat) #make rrbs5910_site_coverage_mat a data frame
+rrbs5910_CG_sites <- rownames_to_column(rrbs5910.df, var = "Site") %>% as_tibble() #make a tibble with row name (CG site) as a column
+rrbs6071.df <- as.data.frame(rrbs6071_site_coverage_mat) #make rrbs6071_site_coverage_mat a data frame
+rrbs6071_CG_sites <- rownames_to_column(rrbs6071.df, var = "Site") %>% as_tibble() #make a tibble with row name (CG site) as a column
+rrbs6125.df <- as.data.frame(rrbs6125_site_coverage_mat) #make rrbs6125_site_coverage_mat a data frame
+rrbs6125_CG_sites <- rownames_to_column(rrbs6125.df, var = "Site") %>% as_tibble() #make a tibble with row name (CG site) as a column 
+
+#BIND ALL RRBS DATA TOGETHER
+rrbs_binded <- full_join(rrbs5910_CG_sites, rrbs6071_CG_sites, by = NULL, copy = FALSE)
+rrbs_binded2 <- full_join(rrbs_binded, rrbs6125_CG_sites, by = NULL, copy = FALSE) 
+rrbs <- rrbs_binded2 %>% gather(Sample, Coverage, 2:47, convert = FALSE) %>% filter(Coverage >= 5)
+#this tibble has site, sample, and coverage, filtered for 5x, and the NAs were removed automatically
+
+#GROUP BY SITES AND COUNT NUMBER OF SAMPLES PER SITE AND MEAN COVERAGE PER SITE IN RRBS
+r5x <- rrbs %>% group_by(Site) %>% summarize(Count_rrbs=n(), mean.cov_rrbs=mean(Coverage))
+#group by site and count the number of samples that cover each and mean coverage of each
+r5x <- r5x %>% mutate(Percent.rrbs=Count_rrbs/46*100) #There are 46 total samples for rrbs
+save(r5x, file = "rrbs_data_for_overlap_plot_5x.rda")
+
+#ADDING CG SITE AS A COLUMN TO ALL ERRBS DATA TIBBLES
+errbs5916.df <- as.data.frame(errbs5916_site_coverage_mat) #make errbs5916_site_coverage_mat a data frame
+errbs5916_CG_sites <- rownames_to_column(errbs5916.df, var = "Site") %>% as_tibble() %>% gather(Sample, Coverage, 2:46,
+     convert = FALSE) %>% filter(Coverage >= 5)
+#make a tibble with row name (CG site) as a column, gather by sample and coverage, filter for 5x
+errbs5975.df <- as.data.frame(errbs5975_site_coverage_mat) #make errbs5975_site_coverage_mat a data frame
+errbs5975_CG_sites <- rownames_to_column(errbs5975.df, var = "Site") %>% as_tibble() %>% gather(Sample, Coverage, 2:41,
+     convert = FALSE) %>% filter(Coverage >= 5)
+#make a tibble with row name (CG site) as a column, gather by sample and coverage, filter for 5x
+errbs6121.df <- as.data.frame(errbs6121_site_coverage_mat) #make errbs6121_site_coverage_mat a data frame
+errbs6121_CG_sites <- rownames_to_column(errbs6121.df, var = "Site") %>% as_tibble() %>% gather(Sample, Coverage, 2:41,
+     convert = FALSE) %>% filter(Coverage >= 5)
+#make a tibble with row name (CG site) as a column, gather by sample and coverage, filter for 5x
+
+#BIND ALL ERRBS DATA TOGETHER
+errbs_binded <- bind_rows(errbs5916_CG_sites, errbs5975_CG_sites) 
+errbs_binded2 <- bind_rows(errbs_binded, errbs6121_CG_sites) 
+
+#GROUP BY SITES AND COUNT NUMBER OF SAMPLES PER SITE AND MEAN COVERAGE PER SITE IN ERRBS
+e5x <- errbs_binded2 %>% group_by(Site) %>% summarize(Count_errbs=n(), mean.cov_errbs=mean(Coverage))
+#group by site and count the number of samples that cover each and mean coverage of each
+e5x <- e5x %>% mutate(Percent.errbs=Count_errbs/125*100) #There are 125 total samples for errbs
+save(e5x, file = "errbs_data_for_overlap_plot_5x.rda")
